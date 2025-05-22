07a8478cbc0a628c522e41d1462204bfe2610844
syed-afnan <afnanblr2001@gmail.com>
2025-05-22 12:34:47 +0530

    my commit

--- anjali.java
+++ anjali.java
@@ -2,7 +2,7 @@ public class anjali {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("my name is anjali");
+        System.out.println("this is my commit");
 
     }
 }
